c694f954bc811e5d1ff978307b772d1dc85a594f
Daniel <despinalm@unal.edu.co>
2021-08-22 16:32:40 -0500

    CU-Iniciar_Sesión

--- app/src/main/java/com/example/unmeet/IniciarSesionActivity.java
+++ app/src/main/java/com/example/unmeet/IniciarSesionActivity.java
@@ -2,15 +2,46 @@ package com.example.unmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.unmeet.R;
+import com.example.unmeet.controller.IniciarSesionController;
+
+import java.util.regex.Pattern;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+  private Button iniciarSesionButton;
+  private IniciarSesionController iniciarSesionController;
+  private EditText correo;
+  private EditText contraseña;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_iniciar_sesion);
+    iniciarSesionButton = findViewById(R.id.iniciar_sesion_button_login);
+    correo = findViewById(R.id.iniciar_sesion_input_correo);
+    contraseña = findViewById(R.id.iniciar_sesion_input_contraseña);
+    iniciarSesionController = new IniciarSesionController();
+
+    iniciarSesionButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+        iniciarSesionController.login(correo.getText().toString(), contraseña.getText().toString());
+
+      }
+    });
+
   }
+
+
+
+
 }
--- app/src/main/java/com/example/unmeet/controller/IniciarSesionController.java
+++ app/src/main/java/com/example/unmeet/controller/IniciarSesionController.java
@@ -1,4 +1,28 @@
 package com.example.unmeet.controller;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
 public class IniciarSesionController {
+
+    public void login(String correo, String contraseña){
+        System.out.println(correo + " " + contraseña);
+        Boolean verCorreo = verificarFormatoCorreo(correo);
+        if(verCorreo){System.out.println("SI ES");}
+
+    }
+
+    public boolean verificarFormatoCorreo(String correo){
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(correo).matches();
+    }
+
+//    public Boolean verificarConexionInternet(){
+//
+//    }
+
 }
